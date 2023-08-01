0b65f2deff753b5ca654a9d3ee3f93b930d667c8
elviswangari <elviswangare@gmail.com>
2023-08-01 17:35:48 +0300

    babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''
+flask babel module
+'''
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config(object):
+    '''
+    config class
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    '''
+    root route
+    '''
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
