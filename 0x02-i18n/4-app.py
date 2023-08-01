df630f38b0836dcb07f2abed5293288a012d8606
elviswangari <elviswangare@gmail.com>
2023-08-01 21:48:39 +0300

    locale and gettext

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,9 +25,12 @@ def get_locale():
     '''
     get and return best match locale
     '''
+    locale = request.args.get("locale")
+    if locale in app.config["LANGUAGES"]:
+        print(locale)
+        return locale
+ 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 @app.route("/")
 def index():
     '''
