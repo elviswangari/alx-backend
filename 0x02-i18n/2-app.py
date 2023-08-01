7452913349046671934ed7b92f596e6402c4f33c
elviswangari <elviswangare@gmail.com>
2023-08-01 19:19:05 +0300

    babel decorator

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 '''
-simple flask babel app
+flask babel module
 '''
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
+class Config(object):
+    '''
+    config class
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
 
@@ -15,3 +26,15 @@ def get_locale():
     get and return best match locale
     '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    '''
+    root route
+    '''
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
